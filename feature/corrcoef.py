a72cc2d69041753feb592dbe763c0542373ef634
eunicecho33 <eunicecho33@gmail.com>
2025-11-22 20:23:34 -0600

    preliminary correlation coefficient matrix heatmap

--- feature/corrcoef.py
+++ feature/corrcoef.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
 
 env = pd.read_csv('./data/environmental_activities.csv')
 rev = pd.read_csv('./data/revenue_distribution_by_sector.csv')
@@ -12,6 +13,9 @@ train = pd.merge(train,
                            'nace_level_2_name'], axis=1),
                 how='inner', on='entity_id')
 
+train['revenue_pct_new'] = train.groupby(['entity_id', 'nace_level_1_code'])['revenue_pct'].transform('sum')
+train = train.drop(['revenue_pct'], axis=1).drop_duplicates(keep='first')
+
 train['scope1_per_usd'] = train['target_scope_1'] / train['revenue'] * 100000
 train['scope2_per_usd'] = train['target_scope_2'] / train['revenue'] * 100000
 train = train[['entity_id',
@@ -19,21 +23,27 @@ train = train[['entity_id',
                'social_score',
                'governance_score',
                'nace_level_1_code',
-               'revenue_pct',
+               'revenue_pct_new',
                'scope1_per_usd',
                'scope2_per_usd']]
 
 train_pivot = train.pivot_table(
     index='entity_id',
     columns='nace_level_1_code', 
-    values='revenue_pct', 
+    values='revenue_pct_new', 
     aggfunc='first'
 )
 
-train = pd.merge(train, train_pivot, how = 'inner', on = 'entity_id').drop(['entity_id', 'revenue_pct', 'nace_level_1_code'], axis=1).drop_duplicates(keep='first').fillna(value=0)
+train = pd.merge(train, train_pivot, how = 'inner', on = 'entity_id').drop(['revenue_pct_new', 'entity_id', 'nace_level_1_code'], axis=1).drop_duplicates(keep='first').fillna(value=0)
 
 train_t = train.transpose()
 
 train_coeff = np.corrcoef(train_t)
+train_coeff = pd.DataFrame(train_coeff, index=train.columns, columns=train.columns)
 
 train_coeff.to_csv('./feature/train_coeff.csv')
+
+fig = sns.heatmap(train_coeff, cbar=True, square=True, xticklabels=False, yticklabels=True, annot=False).get_figure()
+fig.tight_layout()
+fig.savefig('./feature/heatmap.png')
+fig.show()
